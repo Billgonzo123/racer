8f83efa0b823601f57b4e0e5cd81036c3c260ce7
CKURZ <ckurz098@gmail.com>
2022-05-01 16:43:36 -0700

    Added acceleration and breaks

--- racer.js
+++ racer.js
@@ -1,6 +1,7 @@
 
 let keysPressed = [];
 let carDistance = 0;
+let speed = 0;
 function draw() {
     const canvas = document.getElementById('game-window');
     if (canvas.getContext) {
@@ -18,15 +19,23 @@ function draw() {
 draw();
 
 function loop(ctx, w, h) {
-    if (keysPressed.includes('w')) carDistance += 50;
+    if (keysPressed.includes('w'))  {
+        (speed < 100) ? speed += 0.25 : speed  = 100;
+    } else {
+        (speed>0) ? speed -= 0.15 : speed = 0;
+    }
 
+    if (keysPressed.includes('s'))  {
+        (speed>0) ? speed -- : speed = 0;
+    }
+    carDistance += speed;
     //draw road
     for (y = 0; y < h / 2; y++) {
         for (let x = 0; x < w; x++) {
             const perspective = y / (h / 2);
             // grass color
             const grassColor = (Math.sin(20 * Math.pow(1 - perspective, 3) + carDistance * .01) > 0) ? 'green' : 'darkgreen';
-            const clipColor = (Math.sin(20 * Math.pow(1 - perspective, 2) + carDistance * .01) > 0) ? 'red' : 'white';
+            const clipColor = (Math.sin(40 * Math.pow(1 - perspective, 2) + carDistance * .02) > 0) ? 'red' : 'white';
             let color = 'red';
             const midPoint = 0.5;
             let roadWidth = 0.1 + perspective * 0.8;
